83b3379924045ea0e61f3dbff8ab2d4eb8c81046
Ryan <ryanarajo@yahoo.com.br>
2024-05-25 23:20:11 -0300

    feat: construção dos controles, repositórios e selecionadores

---
+++ atvi/src/main/java/com/autobots/automanager/verificadores/StringVerificadorNulo.java
@@ -0,0 +1,14 @@
+package com.autobots.automanager.verificadores;
+
+public class StringVerificadorNulo {
+
+	public boolean verificar(String dado) {
+		boolean nulo = true;
+		if (!(dado == null)) {
+			if (!dado.isBlank()) {
+				nulo = false;
+			}
+		}
+		return nulo;
+	}
+}
